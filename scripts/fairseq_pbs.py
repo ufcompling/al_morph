07d36425b43c21a1b0bd51fa084f97c279532733
Zoey Liu <zoeyliu@ssrb-vpn2-14-45.vpn.ufl.edu>
2024-04-25 17:01:12 -0400

    fairseq workflow

---
+++ scripts/fairseq_pbs.py
@@ -0,0 +1,88 @@
+import io, os
+
+second_string = '''#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=liu.ying@ufl.edu     # Where to send mail	
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --cpus-per-task=1                    
+#SBATCH --mem=8gb                     # Job memory request
+#SBATCH --time=48:00:00               # Time limit hrs:min:sec
+#SBATCH --output=serial_test_%j.log   # Standard output and error log
+#SBATCH --partition=gpu
+#SBATCH --gpus=a100:1
+
+pwd; hostname; date
+
+module load conda
+mamba activate al
+module load fairseq
+
+cd /blue/liu.ying/al_morphseg/
+
+'''
+
+if not os.path.exists('pbs/'):
+	os.system('mkdir pbs/')
+
+lgs = ['btz', 'cho', 'lez', 'ntu', 'tau']
+sizes = ['50', '100', '500', '1000', '1500', '2000']
+
+overall_max_size = 0
+
+for task in ['surSeg', 'surSegGls', 'gls']:
+	for lg in lgs:
+		for size in sizes:
+			select_file = '/blue/liu.ying/al_morphseg/al_trainselect/select.' + lg + '_' + task + size + '.input'
+			max_size = 0
+			with open(select_file) as f:
+				for line in f:
+					max_size += 1
+
+			if max_size > overall_max_size:
+				overall_max_size = max_size
+
+			iterations = int(max_size / 25)
+			for i in range(iterations):
+				select = str(i * 25)
+				for arch in ['transformer']: #, 'transformer_tiny', 'lstm']:
+
+					with open('pbs/' + lg + '_' + task + size + '_select' + select + '_' + arch +'.pbs', 'w') as f:
+						first_string = '''#!/bin/bash\n#SBATCH --job-name=''' + lg + '_' + task + size + '_select' + select + '_' + arch + '    # Job name'
+
+						f.write(first_string + '\n')
+						f.write(second_string + '\n')
+
+						f.write('python scripts/fairseq_wu.py /blue/liu.ying/al_morphseg/al_trainselect/ ' + lg + ' ' + size + ' ' + select + ' ' + arch + ' ' + task + '\n')
+						f.write('\n')
+
+						f.write('module unload fairseq' + '\n')
+						f.write('module load python3' + '\n')
+						f.write('\n')
+						f.write('python scripts/eval.py /blue/liu.ying/al_morphseg/al_trainselect/ ' + lg + ' ' + size + ' ' + select + ' ' + arch + ' ' + task + '\n')
+						f.write('\n')
+			
+						f.write('date' + '\n')
+						f.write('\n')
+
+for task in ['surSeg', 'surSegGls', 'gls']:
+	
+	iterations = int(overall_max_size / 25)
+	for i in range(iterations):
+		select = str(i * 25)
+		for lg in lgs:
+			together_file = open('pbs/' + lg + '_' + task + '_select' + select + '.sh', 'w') # doing sbatch all together
+			for size in sizes:
+				for arch in ['transformer']:
+					if lg + '_' + task + size + '_select' + select + '_' + arch +'.pbs' in os.listdir('pbs/'):
+						together_file.write('sbatch pbs/' + lg + '_' + task + size + '_select' + select + '_' + arch +'.pbs' + '\n')
+'''
+for lg in ['btz']:
+	for task in ['surSeg', 'surSegGls', 'gls']:
+		select = 0
+		while select < 5000:
+			try:
+				os.system('bash pbs/' + lg + '_' + task + '_select' + str(select) + '.sh')
+				print('pbs/' + lg + '_' + task + '_select' + str(select) + '.sh')
+				select += 25
+			except:
+				pass
+'''
---
+++ scripts/gather.py
@@ -0,0 +1,44 @@
+import io, os, sys
+
+datadir = sys.argv[1]
+
+arch = 'transformer'
+
+lgs = ['btz', 'cho', 'lez', 'ntu', 'tau']
+sizes = ['50', '100', '500', '1000', '1500', '2000']
+
+header = ['Language', 'Task', 'Size', 'Select_size', 'Model', 'Metric', 'Value']
+
+for task in ['surSeg']:#, 'surSegGls', 'gls']:
+	outfile = open(task + '_results.txt', 'w')
+	outfile.write(' '.join(tok for tok in header) + '\n')
+	for lg in lgs:
+		for size in sizes:
+			select = 0
+			while select < 100:
+				evaluation_file = datadir + lg + '_' + task + size + '/select' + str(select) + '/' + arch + '/eval.txt'
+				if os.path.exists(evaluation_file):
+					precision = ''
+					recall = ''
+					f1 = ''
+					with open(evaluation_file) as f:
+						for line in f:
+							toks = line.strip().split()
+							if line.startswith('Precision'):
+								precision = toks[1]
+							if line.startswith('Recall'):
+								recall = toks[1]
+							if line.startswith('F1'):
+								f1 = toks[1]
+
+					info = [lg, task, size, select, arch, 'precision', precision]
+					outfile.write(' '.join(str(tok) for tok in info) + '\n')
+					info = [lg, task, size, select, arch, 'recall', recall]
+					outfile.write(' '.join(str(tok) for tok in info) + '\n')
+					info = [lg, task, size, select, arch, 'f1', f1]
+					outfile.write(' '.join(str(tok) for tok in info) + '\n')
+
+				else:
+					print(lg, task, size, select)
+
+				select += 25
